da8d9f97fb72c7d9fc0d555ddc0c286c100001f4
zidane0070608 <53288055+zidane0070608@users.noreply.github.com>
2022-11-30 16:46:38 +0500

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,20 +1,52 @@
 
-const step = 7 + 1; // шаг, где 7 см - между бегунками и 1 см - ширина бегунка
-let widthKarniz = 150; //ширина карниза в см
-const correction = 1.5; // коррекция отступы от края карниза по 0,75см
+// const step = 7 + 1; // шаг, где 7 см - между бегунками и 1 см - ширина бегунка
+// const widthKarniz = 150; //ширина карниза в см
+// const correction = 1.5; // коррекция отступы от края карниза по 0,75см
 
-getCount = (step, widthKarniz) => {
+
+getCount = (widthKarniz) => {
+   const step = 8; // шаг
    let result = Math.round((widthKarniz) / step);
+   console.log(result);
+   return render(widthKarniz, result);
+}
+
+getCenter = (widthKarniz) => {
+   const step = 7 + 1; // шаг
+   let result = Math.round((widthKarniz) / step);
+   let math;
+   if (result % 2) {
+      math = result + 1;
+   } else {
+      math = result
+   }
+   return render(widthKarniz, math);
+}
+
+render = (widthKarniz, result) => {
    const $divResult = document.getElementById('div-result');
-   console.log($divResult.innerHTML = `<hr/> <p>Ширина карниза ${widthKarniz} см </br> Количество ламелей: ${result} шт</p><hr/>`);
-   return result;
+   $divResult.innerHTML = `
+   <hr/>
+   <p>Ширина карниза <strong>${widthKarniz} см</strong></p>
+   <p>Количество ламелей: <strong>${result} шт</strong></p>
+   <hr/>`;
 }
 
 const $btn = document.getElementById('btn-result');
-// console.log($inputValue);
-$btn.addEventListener('click', (e) => {
-   const $inputValue = document.getElementById('width-karniz');
-   let value = $inputValue.value;
-   getCount(step, value);
-   // console.log(result);
+$btn.addEventListener('click', () => {     
+   const $inputChk = document.querySelectorAll('input[type=radio]');
+   const $inputValue = document.getElementById('width-karniz').value; 
+   
+   for (item of $inputChk) {
+      if (item.checked) {
+         if (item.value == 1) {
+            getCount($inputValue);
+            console.log('1');
+         } else {
+            getCenter($inputValue);
+            console.log('2');
+         }         
+      }
+   }
+   
 })
