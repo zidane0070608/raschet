5251f7730ee909bac93a0279c9b9c28d14ee72bc
zidane0070608 <53288055+zidane0070608@users.noreply.github.com>
2022-12-05 11:32:26 +0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,29 +1,47 @@
+'use strict'
 
-// const step = 7 + 1; // шаг, где 7 см - между бегунками и 1 см - ширина бегунка
-// const widthKarniz = 150; //ширина карниза в см
-// const correction = 1.5; // коррекция отступы от края карниза по 0,75см
+const range = (start, stop, step = 1) =>
+   Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+let a = [6, 14, 24, 33, 42, 51]; // где числа в двумерном массиве меньше 7
+let b = 21; // старт размера карниза
+let lamli = 3; // старт количества ламелей
+let d = []; // для создания двумерного массива
 
+for (let i = 0; i < range(1, 61).length; i++) {
+   d.push(range(b, b + 8));
+   b += 8;
+   if (a.includes(i)) {
+      d.push(range(b, b + 7));
+      b += 7;
+   }
+}
 
-getCount = (widthKarniz) => {
-   const step = 8; // шаг
-   let result = Math.round((widthKarniz) / step);
-   console.log(result);
+// console.log(d);
+
+const searchIndexArr = (widthKarniz) => {
+   let result;
+   for(let i = 0; i < d.length; i++) {
+      if (d[i].includes(widthKarniz)) {
+         result = i + lamli;
+         // console.log(result);
+         break;
+      }
+   }
+   return result;
+}
+
+const getLeftRight = (widthKarniz) => {
+   let result = searchIndexArr(widthKarniz);
    return render(widthKarniz, result);
 }
 
-getCenter = (widthKarniz) => {
-   const step = 7 + 1; // шаг
-   let result = Math.round((widthKarniz) / step);
-   let math;
-   if (result % 2) {
-      math = result + 1;
-   } else {
-      math = result
-   }
+const getCenter = (widthKarniz) => {
+   let result = searchIndexArr(widthKarniz);
+   let math = result % 2 ? result + 1 : result;
    return render(widthKarniz, math);
 }
 
-render = (widthKarniz, result) => {
+const render = (widthKarniz, result) => {
    const $divResult = document.getElementById('div-result');
    $divResult.innerHTML = `
    <hr/>
@@ -33,20 +51,21 @@ render = (widthKarniz, result) => {
 }
 
 const $btn = document.getElementById('btn-result');
-$btn.addEventListener('click', () => {     
+$btn.addEventListener('click', () => {
    const $inputChk = document.querySelectorAll('input[type=radio]');
-   const $inputValue = document.getElementById('width-karniz').value; 
-   
-   for (item of $inputChk) {
+   const $inputValue = document.getElementById('width-karniz').value;
+   // console.log(Number($inputValue), 'dawdwa');
+
+   for (let item of $inputChk) {
       if (item.checked) {
-         if (item.value == 1) {
-            getCount($inputValue);
-            console.log('1');
+         if (item.value === '1') {
+            getLeftRight(Number($inputValue));
+            // console.log(item.value);
          } else {
-            getCenter($inputValue);
-            console.log('2');
-         }         
+            getCenter(Number($inputValue));
+            // console.log(item.value);
+         }
       }
    }
-   
+
 })
